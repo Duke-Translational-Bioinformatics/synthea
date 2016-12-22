b66ee655e29595eb118178d0dfa44291a1807a73
Carlton Duffett <cjduffett@comcast.net>
2016-12-21 21:02:17 -0500

    Updated Package and tests
    Updated the Package to better support loading contexts. Added unit tests for the new Package class.

--- lib/generic/package.rb
+++ lib/generic/package.rb
@@ -1,15 +1,14 @@
 module Synthea
   module Generic
     class Package
-      attr_reader :name, :modules, :main
+      attr_reader :name, :main, :submodules
 
       def initialize(package_path)
-        @name = package_path[%r{\/(\w+)\/*$}, 1]
-        @modules = []
-        @main = {}
+        @path = package_path
+        @name = get_name(package_path)
 
-        files = Dir.glob(File.join(package_path, '*.json'))
-        main_file = File.join(package_path, @name + '.json')
+        files = Dir.glob(get_path('*.json'))
+        main_file = get_path(@name + '.json')
         # The package must include a JSON module file with the same name.
         # That file is the "main" file for the package and its context is
         # run first.
@@ -17,20 +16,36 @@ module Synthea
           raise "No main module \"#{@name}.json\" found in package \"#{@name}\""
         end
 
-        files.each do |file|
-          m = load_module(file)
-          unless m['package'] && m['package'] == @name
-            raise "Module \"#{m['name']}\" must be part of package \"#{@name}\""
+        files.each do |filename|
+          context = get_context(filename)
+          unless context.package && context.package == @name
+            raise "Module \"#{context.name}\" must be part of package \"#{@name}\""
           end
-          @main = m if file == main_file
-          @modules << m
+          if filename == main_file
+            @main = context
+          else
+            @submodules ||= {}
+            @submodules[context.name] = context
+          end
         end
       end
 
-      def load_module(file)
-        m = JSON.parse(File.read(file))
-        puts "Loaded \"#{m['name']}\" module from package \"#{@name}\""
-        m
+      def get_context(fullpath)
+        context = Synthea::Generic::Context.new(get_config(fullpath))
+        puts "Loaded \"#{context.name}\" module from package \"#{@name}\""
+        context
+      end
+
+      def get_config(fullpath)
+        JSON.parse(File.read(fullpath))
+      end
+
+      def get_path(filename)
+        File.join(@path, filename)
+      end
+
+      def get_name(package_path)
+        package_path[%r{\/(\w+)\/*$}, 1]
       end
     end
   end
---
+++ test/unit/generic_package_test.rb
@@ -0,0 +1,87 @@
+require_relative '../test_helper'
+
+class GenericPackageTest < Minitest::Test
+
+  def setup
+    @time = Time.now
+    @patient = Synthea::Person.new
+    @patient[:gender] = 'F'
+    @patient.events.create(@time - 35.years, :birth, :birth)
+    @patient[:age] = 35
+  end
+
+  def test_new_package
+    # Load the package and test that it loaded correctly.
+    package = get_package('test_package')
+    assert_equal("test_package", package.name)
+    assert_equal(1, package.submodules.length)
+
+    # Test the the package has the correct main context
+    # to start execution from.
+    main = package.main
+    assert_equal("Test Package Main Module", main.name)
+    assert_equal("test_package", main.package)
+    assert_equal([], main.history)
+    assert_equal("Initial", main.current_state.name)
+    assert_equal(0, main.args.keys.length)
+
+    # Test that the package's submodule loaded correctly.
+    submodule = package.submodules["Test Package Submodule"]
+    assert_equal("Test Package Submodule", submodule.name)
+    assert_equal("test_package", submodule.package)
+    assert_equal([], submodule.history)
+    assert_equal("Initial", submodule.current_state.name)
+    assert_equal(0, submodule.args.keys.length)
+  end
+
+  def test_run_package
+    package = get_package('test_package')
+    main = package.main
+
+    # The main context should first block at the CallSubmodule.
+    blocking_state = main.run(@time, @patient)
+    assert_equal("CallSubmodule", blocking_state.name)
+    assert_equal("Test Package Submodule", blocking_state.submodule)
+    expected_args = {
+      "condition" => "Examplitis",
+      "encounter" => "Encounter"
+    }
+    assert_equal(expected_args, blocking_state.args)
+
+    # Running the submodule that's called should block at it's Terminal state.
+    submodule = package.submodules["Test Package Submodule"]
+    submodule.history = main.history
+    submodule.args = blocking_state.args
+    blocking_state = submodule.run(@time, @patient)
+    assert_equal("Sub_Terminal", blocking_state.name)
+
+    # Resuming the main should then block at it's terminal state.
+    blocking_state = main.run(@time, @patient)
+    assert_equal("Main_Terminal", blocking_state.name)
+
+    # Verify that the patient's record was updated correctly.
+    encounters = @patient.record_synthea.encounters
+    assert_equal(1, encounters.length)
+
+    conditions = @patient.record_synthea.conditions
+    assert_equal(1, conditions.length)
+    assert_equal(:acute_examplitis, conditions[0]['type'])
+
+    # Verify that the patient's MedicationOrder used the submodule's
+    # args correctly.
+    medications = @patient.record_synthea.medications
+    assert_equal(1, medications.length)
+    assert_equal(@time, medications[0]['start_time'])
+    assert_equal(:acute_examplitis, medications[0]['reasons'][0])
+  end
+
+  def get_package(package_path)
+    Synthea::Generic::Package.new(get_path(package_path))
+  end
+
+  def get_path(file_or_dir)
+    fixture_dir = File.expand_path('../../fixtures/generic', __FILE__)
+    File.join(fixture_dir, file_or_dir)
+  end
+
+end
